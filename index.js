1a2e90f34e7811c0ecb85c141a819a94482d7447
Shakira Weir <shakira.weir87@gmail.com>
2019-01-18 16:01:20 -0500

    added hbs and views

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.set("view engine", "hbs");
+app.get("/:name", (req, res) => {
+  res.render('index', { name: req.params.name })
 });
-
 
 
 
